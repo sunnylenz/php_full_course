81fe91e1bf116e33af485fbed318c3afdd18fa3f
sunnylearne <ikesundayshield@gmail.com>
2020-11-05 11:00:05 +0100

    single page form processing

---
+++ globe_bank/public/staff/subjects/edit.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once('../../../private/initialize.php');
+
+if(!isset($_GET['id'])){
+    redirect_to(url_for('/staff/subjects/new.php'));  
+}
+$id = $_GET['id'];
+if(is_post_request()){
+    // Handle form values sent by new.php
+
+$menu_name = $_POST['menu_name'] ?? '';
+$position = $_POST['position'] ?? '';
+$visible = $_POST['visible'] ?? '';
+
+echo "Form parameters<br>";
+echo "Menu name: " . $menu_name . "<br>";
+echo "Position: " . $position . "<br>";
+echo "Visible: " . $visible . "<br>";
+
+} else {
+    // redirect_to(url_for('/staff/subjects/new.php'));  
+}
+
+
+// $test = $_GET['test'] ?? '';
+
+// if($test == '404'){
+//     error_404();
+// } elseif($test == '500'){
+//     error_500();
+// } elseif($test == 'redirect'){
+//     redirect_to(url_for('/staff/subjects/index.php'));
+// }
+?>
+
+
+<?php $page_title = 'Edit Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+<a href="<?php echo url_for('/staff/subjects/index.php'); ?>" class="back-link">&laquo; Back to List</a>
+
+<div class="subject edit">
+    <h1>Edit Subject</h1>
+    <form action="<?php echo url_for('/staff/subjects/edit.php?id='. h(u($id)) ); ?>" method="post">
+
+        <dl>
+            <dt>Menu Name</dt>
+            <dd><input type="text" name="menu_name" value=""></dd>
+        </dl>
+
+        <dl>
+            <dt>Position</dt>
+            <dd>
+                <select name="position">
+                    <option value="1">1</option>
+                </select>
+            </dd>
+        </dl>
+
+        <dl>
+            <dt>Visible</dt>
+            <dd>
+                <input type="hidden" name="visible" value="0">
+                <input type="checkbox" name="visible" value="1">
+            </dd>
+        </dl>
+
+        <div id="operations">
+            <input type="submit" value="Edit Subject">
+        </div>
+
+
+    </form>
+</div>
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
