63a54c732ccb91df8491e7bb57150bb6c73c44da
sunnylearne <ikesundayshield@gmail.com>
2020-12-02 15:26:02 +0100

    skipping hidden subjects and pages

--- globe_bank/private/shared/public_navigation.php
+++ globe_bank/private/shared/public_navigation.php
@@ -8,6 +8,7 @@
   <?php $nav_subjects = find_all_subjects(); ?>
   <ul class="subjects">
     <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { ?>
+      <?php if(!$nav_subject['visible']) { continue; }?>
       <li class="<?php if($nav_subject['id'] == $subject_id) { echo 'selected'; } ?>">
         <a href="<?php echo url_for('index.php?subject_id=' . h(u($nav_subject['id']))); ?>">
           <?php echo h($nav_subject['menu_name']); ?>
@@ -17,6 +18,7 @@
           <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
           <ul class="pages">
             <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
+              <?php if(!$nav_page['visible']) { continue; }?>
               <li class="<?php if($nav_page['id'] == $page_id) { echo 'selected'; } ?>">
                 <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
                   <?php echo h($nav_page['menu_name']); ?>
